644a614b39b19064986a6fa863ab9a7a982dc0d0
Nathan Gelman <nathanjlg@gmail.com>
2024-01-12 21:11:27 -0800

    added recaptcha site key

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
+import ReCAPTCHA from "react-google-recaptcha";
 
 function App() {
+  const siteKey: string = process.env.CAPTCHA_SITE_KEY || "";
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello, world!</h1>
+        <ReCAPTCHA sitekey={siteKey} />
       </header>
     </div>
   );
